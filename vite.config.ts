9de59e348ed21d21651cff214f49667125754084
unknown <amroaarsi@gmail.com>
2026-05-05 00:11:49 +0300

    new updates

--- vite.config.ts
+++ vite.config.ts
@@ -9,12 +9,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  // server: {
-  //   host: '0.0.0.0',
-  //   port: 5173,
-  //   strictPort: true,
-  //   allowedHosts: ['alyaseenerp.com', 'www.alyaseenerp.com'],
-  // }
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ['alyaseenerp.com', 'www.alyaseenerp.com'],
+  }
 })
 
 
